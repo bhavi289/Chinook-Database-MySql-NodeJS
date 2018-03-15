15ae242103b5006c663c41c0445dd6c8605b2d4c
bhavi289 <bhavi.c16@iiits.in>
2018-03-16 00:48:53 +0530

    final app and report

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'chinook_full_index'
+    database : 'chinook_no_index'
 });
 
 // Connect
@@ -72,7 +72,7 @@ app.get('/join3', (req, res) => {
 
 // Join Querry
 app.get('/Playlists', (req, res) => {
-    let sql = 'SELECT  PlaylistTrack.PlaylistId,Playlist_name, TrackName,Composer,AlbumTitle,ArtistName,MediaTypeName,GenreName,Milliseconds,Bytes,UnitPrice FROM PlaylistTrack INNER JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id) p ON PlaylistTrack.TrackId = p.TrackId';
+    let sql = 'SELECT  PlaylistTrack.PlaylistId,Playlist_name, TrackName,Composer,AlbumTitle,ArtistName,MediaTypeName,GenreName,Milliseconds,Bytes,TrackPrice FROM PlaylistTrack INNER JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id) p ON PlaylistTrack.TrackId = p.Track_Id';
     let query = db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
@@ -81,7 +81,7 @@ app.get('/Playlists', (req, res) => {
 });
 // Join Querry
 app.get('/Playlist/:id', (req, res) => {
-    let sql = `SELECT * FROM PlaylistTrack INNER JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id) p ON PlaylistTrack.TrackId = p.TrackId where PlaylistTrack.PlaylistId="${req.params.id}"`;
+    let sql = `SELECT * FROM PlaylistTrack INNER JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id) p ON PlaylistTrack.TrackId = p.Track_Id where PlaylistTrack.PlaylistId="${req.params.id}"`;
     let query = db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
@@ -97,8 +97,8 @@ app.get('/Employee', (req, res) => {
         res.json(result);
     });
 });
-
-app.get('/khtarnak_join', (req, res) => {
+// Join Querry
+app.get('/Customers', (req, res) => {
     let sql = 'SELECT * FROM(SELECT * FROM InvoiceLine INNER JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.Invoice_Id INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id)k ON InvoiceLine.TrackId = k.Track_Id)y INNER JOIN Customer ON y.CustomerId=Customer.Customer_Id';
     let query = db.query(sql, (err, result) => {
         if(err) throw err;
@@ -106,9 +106,9 @@ app.get('/khtarnak_join', (req, res) => {
         res.json(result);
     });
 });
-
-app.get('/khtarnak_join2', (req, res) => {
-    let sql = 'SELECT  PlaylistTrack.PlaylistId,Playlist_name, TrackName,Composer,AlbumTitle,ArtistName,MediaTypeName,GenreName,Milliseconds,Bytes,TrackPrice FROM PlaylistTrack INNER JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id) p ON PlaylistTrack.TrackId = p.Track_Id';
+// Join Querry
+app.get('/Customer/:id', (req, res) => {
+    let sql = `SELECT * FROM(SELECT * FROM InvoiceLine INNER JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.Invoice_Id INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id)k ON InvoiceLine.TrackId = k.Track_Id)y INNER JOIN Customer ON y.CustomerId=Customer.Customer_Id where Customer.Customer_Id ="${req.params.id}"`;
     let query = db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
@@ -116,11 +116,29 @@ app.get('/khtarnak_join2', (req, res) => {
     });
 });
 
+// Join Querry
+app.get('/BestCustomer', (req, res) => {
+    let sql = 'select CustomerId, count(InvoiceId) as NoOfTracks, SUM(UnitPrice) as Bill FROM(SELECT * FROM InvoiceLine INNER JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.Invoice_Id INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id)k ON InvoiceLine.TrackId = k.Track_Id)y INNER JOIN Customer ON y.CustomerId=Customer.Customer_Id GROUP BY Customer_Id ORDER BY Bill DESC';
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+});
+// Join Querry
+app.get('/TopTracks', (req, res) => {
+    let sql = 'select TrackId,TrackName,ArtistName,AlbumTitle,Composer,TrackPrice,MediaTypeName,GenreName, count(TrackId) as SoldCopies FROM(SELECT * FROM InvoiceLine INNER JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.Invoice_Id INNER JOIN (SELECT * FROM Track INNER JOIN (SELECT * FROM Album INNER JOIN Artist ON Album.Artist_Id = Artist.ArtistId) z ON Track.AlbumId = z.Album_Id INNER JOIN MediaType ON Track.MediaTypeId = MediaType.MediaType_Id INNER JOIN Genre ON Track.GenreId = Genre.Genre_Id)k ON InvoiceLine.TrackId = k.Track_Id)y INNER JOIN Customer ON y.CustomerId=Customer.Customer_Id where GenreName = "Rock" GROUP BY TrackId ORDER BY SoldCopies DESC';
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+});
 });
 app.get('/', (req, res) => {
         res.send('Hello');
 });
 
-app.listen('3006', () => {
+app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
